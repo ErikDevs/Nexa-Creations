2b193226c397945b5eca8c1caea536778caee312
simplex <simplexerick@gmail.com>
2024-01-28 23:43:33 +0300

    new changes

--- app/layout.js
+++ app/layout.js
@@ -1,45 +1,26 @@
-"use client"
-import { DM_Sans, Inter, Poppins, Sora } from 'next/font/google'
-import './globals.css'
-import Navbar from '@/components/navbar'
-import Footer from '@/components/footer'
-import { useState } from 'react'
-import Provider from '@/sessionProvider'
+import { Sora } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
+const sora = Sora({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
 
-const sora = Sora({ subsets: ['latin'], weight: [ "100", "200", "300", "400", "500", "600", "700", "800"]})
-
-
-
-// export const metadata = {
-//   title: 'Nexa Creation',
-//   description: 'Website to showcase our services',
-// }
+export const metadata = {
+  title: "Nexa Creation",
+  description: "Website to showcase our services",
+};
 
 export default function RootLayout({ children }) {
-
-  const [loading, setLoading] = useState(true)
-
-
-    setTimeout(() => { setLoading(false)}, 3000)
-  
   return (
     <html lang="en">
       <body className={`${sora.className} bg-slate-50`}>
-        {loading ? <div className='flex justify-center bg-slate-300 min-h-screen m-auto items-center'>
-        <div className='loader'></div>
-        </div> : <>
-          <Provider>
-          <Navbar  />
-          {children}
-          <Footer />  
-          </Provider>
-
-          
-          </>
-        }
-        
-        </body>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
