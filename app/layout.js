37355d3cb670b65c03eb80c544e601d58bd256ff
simplex <simplexerick@gmail.com>
2024-02-03 20:56:44 +0300

    fixed error

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { monstserrat } from "./fonts/fonts";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "Nexa Creation",
