ed02559dac3252e80b1ad8a25778aef949e12d18
simplex <simplexerick@gmail.com>
2024-02-03 21:08:10 +0300

    fixed error

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
-
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
+};
