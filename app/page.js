5f9de98c815200ef86323d3101bbde3a79e1f505
simplex <simplexerick@gmail.com>
2024-02-03 21:28:12 +0300

    make one change

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
-import AboutUs from "@/app/components/AboutUs";
-import Contact from "@/app/components/Contact";
-import Hero from "@/app/components/Hero";
-import Introduction from "@/app/components/Introduction";
-import Ourphilosophy from "@/app/components/Ourphilosophy";
-import Projects from "@/app/components/Projects";
-import Testimonials from "@/app/components/Testimonials";
-import WhyChooseUs from "@/app/components/WhyChooseUs";
+import AboutUs from "./components/AboutUs";
+import Contact from "./components/Contact";
+import Hero from "./components/Hero";
+import Introduction from "./components/Introduction";
+import Ourphilosophy from "./components/Ourphilosophy";
+import Projects from "./components/Projects";
+import Testimonials from "./components/Testimonials";
+import WhyChooseUs from "./components/WhyChooseUs";
 
 export default function Home() {
   return (
