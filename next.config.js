8448b1f9b6cd46bed8301de3d54567c8c511231b
simplex <simplexerick@gmail.com>
2024-02-03 21:19:17 +0300

    make one change

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-  webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname, "src");
-    return config;
-  },
-};
+module.exports = nextConfig;
